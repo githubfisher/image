843b87862d040f32c8353f0ca06e31498959aa33
githubfisher <gooodfisher@126.com>
2018-10-26 11:51:42 +0800

    fisher:增加合成图片/文字/输出字符串的方法

--- src/Image.php
+++ src/Image.php
@@ -15,13 +15,42 @@ class Image
      * @var resource
      */
     protected $resource = null;
+	/**
+	 * 图片宽度
+	 * @var $width
+	 */
+	protected $width    = 750;
+	/**
+	 * 图片高度
+	 * @var $height
+	 */
+	protected $height   = 1334;
+	/**
+	 * 图片颜色 默认黑色
+	 * @var $color
+	 */
+	protected $color    = [0, 0, 0];
+	/**
+	 * 图片alpha
+	 * @var $alpha
+	 */
+	protected $alpha    = 127;
 
-    /**
-     * Image constructor.
-     * @param $image
-     */
-    public function __construct($image = null)
+	/**
+	 * Image constructor.
+	 * @param null $image
+	 * @param null $width
+	 * @param null $height
+	 * @param null $color
+	 * @param null $alpha
+	 */
+    public function __construct($image = null, $width = null, $height = null, $color = null, $alpha = null)
     {
+	    isset($width)  && $this->width  = $width;
+	    isset($height) && $this->height = $height;
+	    isset($color)  && $this->color  = $color;
+	    isset($alpha)  && $this->alpha  = $alpha;
+
         $this->init($image);
     }
 
@@ -32,6 +61,8 @@ class Image
     {
         if ($image !== null && !is_resource($image)) {
             $image = Resources::instance()->get($image);
+        } else {
+	        $image = Resources::instance()->create($this->width, $this->height);
         }
 
         $this->resource = $image;
@@ -46,11 +77,15 @@ class Image
     }
 
     /**
+     * @param bool $download 是否下载 默认否
      * @param null $extend
      */
-    public function output($extend = null)
+    public function output($extend = null, $download = false)
     {
-        ob_clean();
+	    if ($download) {
+		    header('Content-type', 'charset=utf-8;application/x-' . $extend);
+		    header('Content-Disposition:attachment');
+	    }
 
         switch ($extend) {
             case 'png':
@@ -80,8 +115,6 @@ class Image
      */
     public function save($filename, $extend = null, $quality = 100)
     {
-        ob_clean();
-
         switch ($extend) {
             case 'png':
                 imagepng($this->resource, $filename, $quality);
@@ -213,4 +246,94 @@ class Image
 
         return $this->resource;
     }
+
+	/**
+	 * 合成图片
+	 * @param null $image 需要合成到底图的图片
+	 * @param null $extend 后缀名或网络类型标识
+	 * @param int $width 目标图片宽度 (合成到底图上时, 根据目标宽高, 可能会对原图拉伸或压缩)
+	 * @param int $height 目标图片高度
+	 * @param int $top 坐标y
+	 * @param int $left 坐标x
+	 * @param null $originWidth 原始图片宽度 (指需要合成到底图的图片) 默认跟随目标宽高
+	 * @param null $originHeight 原始图片高度
+	 * @return resource 底图
+	 * @author fisher
+	 * @date 2018/10/26 上午10:38
+	 */
+	public function mergeImage($image = null, $extend = null, $width = 0, $height = 0, $top = 0, $left = 0, $originWidth = null, $originHeight = null)
+	{
+		if ( ! is_resource($image)) {
+			$image = Resources::instance()->get($image);
+		}
+
+		$originHeight = isset($originHeight) ? $originHeight : $height;
+		$originWidth  = isset($originWidth) ? $originWidth : $width;
+
+		imagecopyresized($this->resource, $image, $left, $top, 0, 0, $width, $height, $originWidth, $originHeight);
+
+		return $this->resource;
+	}
+
+	/**
+	 * 合成文字 - 在底图基础上添加文字
+	 * @param $text //文字内容
+	 * @param null $font //字体文件地址 绝对路径
+	 * @param int $size 文字大小
+	 * @param int $top 坐标y
+	 * @param int $left 坐标x
+	 * @param array $color 颜色值 rgb数组
+	 * @param int $rotation 旋转角度 默认0不旋转
+	 * @return resource 底图
+	 * @author fisher
+	 * @date 2018/10/26 上午10:38
+	 */
+	public function mergeText($text, $font = null, $size = 16, $top = 0, $left = 0, $color = [], $rotation = 0)
+	{
+		$color = empty($color) ? $this->color : $color;
+		$color = imagecolorallocate($this->resource, $color['0'], $color['1'], $color['2']);
+		imagefttext($this->resource, $size, $rotation, $left, $top, $color, $font, $text);
+
+		return $this->resource;
+	}
+
+	/**
+	 * 输出字符串 默认base64
+	 * @param string $extend
+	 * @param string $encode
+	 * @return string
+	 * @author fisher
+	 * @date 2018/10/26 上午11:43
+	 */
+	public function toString($extend = 'png', $encode = 'base64')
+	{
+		ob_start();
+
+		switch ($extend) {
+			case 'jpg':
+			case 'jpeg':
+				imagejpeg($this->resource);
+
+				break;
+			case 'gif':
+				imagegif($this->resource);
+
+				break;
+			default:
+				imagepng($this->resource);
+
+				break;
+		}
+
+		$resource = ob_get_clean();
+
+		if ($encode === 'base64') {
+			$prefix   = 'data:image/' . $extend . ';base64,';
+			$resource = $prefix . base64_encode($resource);
+		}
+
+		imagedestroy($this->resource);
+
+		return $resource;
+	}
 }
